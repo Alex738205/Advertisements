8c32e801217bd771cd7870f61c317ba7fa2127c2
Alex <Alex738205@yandex.ru>
2023-07-25 20:55:12 +0400

    add model Advertisement

--- models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Advertisements(models.Model):
-    title = models.CharField('Заголовок', max_Length = 128)
-    description = models.TextField('Описание')
-    price = models.DecimalField('Цена',max_digits=10, decimal_places=2)
-    auction = models.BooleanField('Торг',help_text='Торг уместен')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
